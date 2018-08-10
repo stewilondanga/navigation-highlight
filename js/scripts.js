75bac7bb8cdd7a44929fcea53a54019755c5a4fd
Stewart ilondanga <stilondanga@gmail.com>
2018-08-10 12:15:48 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,18 +14,18 @@ const options = $('.options')
 const bar = $('.bar')
 let focus = 0;
 allOptions.forEach((option, i) => {
-      /*  option.addEventListener('click', () => {
-          if (focus !== i) {
-            bar.className = 'bar';
-            options.className = 'options';
-            if (focus < i) {
-              bar.classList.add('right')
-            } else {
-              bar.classList.add('left')
-            }
-            bar.classList.add(`index${i}`)
-            options.classList.add(`index${i}`)
-            focus = i;
-          }
-        })
-      })
+  option.addEventListener('click', () => {
+    if (focus !== i) {
+      bar.className = 'bar';
+      options.className = 'options';
+      if (focus < i) {
+        bar.classList.add('right')
+      } else {
+        bar.classList.add('left')
+      }
+      bar.classList.add(`index${i}`)
+      options.classList.add(`index${i}`)
+      focus = i;
+    }
+  })
+})
