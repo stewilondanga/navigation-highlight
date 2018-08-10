05fa75e897cb97862b4522256ad849af7285902d
Stewart ilondanga <stilondanga@gmail.com>
2018-08-10 12:13:19 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
-var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+/*var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+window.$ = document.querySelector.bind(document);
+window.$$ = document.querySelectorAll.bind(document);
+const allOptions = $$('.option')
+const options = $('.options')
+const bar = $('.bar')
+let focus = 0;
+allOptions.forEach((option, i) => {
+  option.addEventListener('click', () => {
+    if (focus !== i) {
+      bar.className = 'bar';
+      options.className = 'options';
+      if (focus < i) {
+        bar.classList.add('right')
+      } else {
+        bar.classList.add('left')
+      }
+      bar.classList.add(`index${i}`)
+      options.classList.add(`index${i}`)
+      focus = i;
+    }
+  })
+})
